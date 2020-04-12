13a555f46c2b6ad7bea9eef2a3470ff50f77dc1b
dh <hyunlove12@naver.com>
2020-04-13 02:17:48 +0900

    로그인 처리 / route

--- bootMovieRating/src/main/java/com/config/AuthenticationFilter.java
+++ bootMovieRating/src/main/java/com/config/AuthenticationFilter.java
@@ -23,46 +23,46 @@ public class AuthenticationFilter extends AbstractAuthenticationProcessingFilter
   private static final Logger log = LoggerFactory.getLogger(AuthenticationFilter.class);
 
   public AuthenticationFilter() {
-    super(new AntPathRequestMatcher("/api/authentications", "POST"));
+      super(new AntPathRequestMatcher("/api/authentications", "POST"));
   }
 
   @Override
   public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException {
 
-    log.debug("Processing login request");
+      log.debug("Processing login request");
 
-    String requestBody = IOUtils.toString(request.getReader());
-    LoginRequest loginRequest = JsonUtils.toObject(requestBody, LoginRequest.class);
-    if (loginRequest == null || loginRequest.isInvalid()) {
-    	throw new InsufficientAuthenticationException("Invalid authentication request");
-    }
+      String requestBody = IOUtils.toString(request.getReader());
+      LoginRequest loginRequest = JsonUtils.toObject(requestBody, LoginRequest.class);
+      if (loginRequest == null || loginRequest.isInvalid()) {
+    	  throw new InsufficientAuthenticationException("Invalid authentication request");
+      }
 
-    UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(loginRequest.username, loginRequest.password);
-    	return this.getAuthenticationManager().authenticate(token);
+      UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(loginRequest.username, loginRequest.password);
+    	  return this.getAuthenticationManager().authenticate(token);
   }
 
   static class LoginRequest {
-    private String username;
-    private String password;
+      private String username;
+      private String password;
 
-    public boolean isInvalid() {
-    	return StringUtils.isBlank(username) || StringUtils.isBlank(password);
-    }
+      public boolean isInvalid() {
+    	  return StringUtils.isBlank(username) || StringUtils.isBlank(password);
+      }
 
-    public String getUsername() {
-    	return username;
-    }
+      public String getUsername() {
+    	  return username;
+      }
 
-    public void setUsername(String username) {
-    	this.username = username;
-    }
+      public void setUsername(String username) {
+    	  this.username = username;
+      }
 
-    public String getPassword() {
-    	return password;
-    }
+      public String getPassword() {
+    	  return password;
+      }
 
-    public void setPassword(String password) {
-    	this.password = password;
-    }
+      public void setPassword(String password) {
+    	  this.password = password;
+      }
   }
 }
